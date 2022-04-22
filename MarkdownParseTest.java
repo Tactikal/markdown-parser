5059c3db768c2242c3a8568978b011926d0cf93f
Tactikal <veig.anon99@gmail.com>
2022-04-22 12:48:33 -0700

    passed test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,31 @@
 import static org.junit.Assert.*;  //imports the JUnit assert method
 import org.junit.*;                //imports JUnit
+import java.nio.file.Files;
+import java.util.*;
+import java.nio.file.Path;
+import java.io.IOException;
+
 		
 public class MarkdownParseTest {   //creates the class for testing
 @Test						       //indicates that this is a test block
     public void addition() {	   //creates a test method that tests different cases
     assertEquals(2, 1 + 1);		   //checks if the first input is equal to the second input
     } 
+
+@Test 
+    public void testGetLinks() {
+        Path filePath = Path.of("test-file.md");
+        try {
+            String file = Files.readString(filePath);
+            List<String> result = MarkdownParse.getLinks(file);
+            List<String> expected = List.of("https://something.com", "some-page.html");
+            assertEquals("Lists should be equal", expected, result);
+        }
+
+        catch (Exception e) {
+            System.out.println("Exception was thrown and caught");
+        }
+        
+
+    }
 }
